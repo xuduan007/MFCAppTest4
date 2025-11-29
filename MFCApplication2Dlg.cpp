aca27d48a39f930cec7936306267a43ec08ef122
xuduan <16618860@qq.com>
2025-11-30 00:04:35 +0800

    The code trae generated

--- MFCApplication2Dlg.cpp
+++ MFCApplication2Dlg.cpp
@@ -1,4 +1,4 @@
-﻿
+
 // MFCApplication2Dlg.cpp: 实现文件
 //
 
@@ -47,12 +47,220 @@ BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
 END_MESSAGE_MAP()
 
 
+// CCustomMessageBox 实现
+CCustomMessageBox::CCustomMessageBox(const CString& message, COLORREF textColor, CWnd* pParent)
+	: CDialogEx(IDD_CUSTOM_MESSAGE_BOX, pParent), m_message(message), m_textColor(textColor)
+{
+}
+
+BOOL CCustomMessageBox::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+	m_messageText.SetWindowText(m_message);
+	return TRUE;
+}
+
+CCustomMessageBox::~CCustomMessageBox()
+{
+}
+
+void CCustomMessageBox::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_MESSAGE_TEXT, m_messageText);
+}
+
+BEGIN_MESSAGE_MAP(CCustomMessageBox, CDialogEx)
+	ON_WM_CTLCOLOR()
+END_MESSAGE_MAP()
+
+HBRUSH CCustomMessageBox::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	HBRUSH hbr = CDialogEx::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	if (pWnd->GetDlgCtrlID() == IDC_MESSAGE_TEXT)
+	{
+		pDC->SetTextColor(m_textColor);
+		pDC->SetBkColor(GetSysColor(COLOR_WINDOW));
+		return (HBRUSH)GetStockObject(WHITE_BRUSH);
+	}
+
+	return hbr;
+}
+
+// CCaptchaDlg 实现
+CCaptchaDlg::CCaptchaDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CAPTCHA_DIALOG, pParent)
+{
+}
+
+void CCaptchaDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_CAPTCHA_DISPLAY, m_captchaDisplay);
+	DDX_Control(pDX, IDC_CAPTCHA_INPUT, m_captchaInput);
+}
+
+BEGIN_MESSAGE_MAP(CCaptchaDlg, CDialogEx)
+	ON_WM_TIMER()
+	ON_EN_CHANGE(IDC_CAPTCHA_INPUT, &CCaptchaDlg::OnEnChangeCaptchaInput)
+	ON_BN_CLICKED(IDC_CLOSE_CAPTCHA, &CCaptchaDlg::OnBnClickedCloseCaptcha)
+	ON_WM_CTLCOLOR()
+END_MESSAGE_MAP()
+
+BOOL CCaptchaDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置欢迎主题图片
+	CBitmap bitmap;
+	bitmap.LoadBitmap(IDB_BITMAP1);
+	CStatic* pStatic = (CStatic*)GetDlgItem(IDC_STATIC);
+	pStatic->ModifyStyle(0xf, SS_BITMAP | SS_CENTERIMAGE);
+	pStatic->SetBitmap(bitmap);
+
+	// 生成初始验证码
+	GenerateCaptchaCode();
+	
+	// 设置验证码显示的字体和颜色
+	CFont font;
+	font.CreatePointFont(120, _T("Arial"));
+	m_captchaDisplay.SetFont(&font);
+
+	// 设置输入框只能输入数字
+	m_captchaInput.LimitText(6);
+
+	// 启动定时器，每3秒更新一次验证码
+	SetTimer(1, 3000, nullptr);
+
+	return TRUE;
+}
+
+HBRUSH CCaptchaDlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	HBRUSH hbr = CDialogEx::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	return hbr;
+}
+
+void CCaptchaDlg::GenerateCaptchaCode()
+{
+	// 生成6位随机数字
+	CString code;
+	for (int i = 0; i < 6; i++)
+	{
+		code += static_cast<TCHAR>(_T('0') + rand() % 10);
+	}
+	m_captchaCode = code;
+	m_captchaDisplay.SetWindowText(code);
+}
+
+void CCaptchaDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	if (nIDEvent == 1)
+	{
+		GenerateCaptchaCode();
+	}
+	CDialogEx::OnTimer(nIDEvent);
+}
+
+void CCaptchaDlg::OnEnChangeCaptchaInput()
+{
+	CString input;
+	m_captchaInput.GetWindowText(input);
+	
+	if (input.GetLength() == 6)
+	{
+		CMFCApplication2Dlg* pMainDlg = (CMFCApplication2Dlg*)GetParent();
+		if (input == m_captchaCode)
+{
+	// 验证成功
+	CCustomMessageBox dlg(_T("验证正确"), RGB(0, 255, 0), this);
+	dlg.DoModal();
+	pMainDlg->UpdateStatistics(true);
+	m_captchaInput.SetWindowText(_T(""));
+	GenerateCaptchaCode();
+}
+else
+{
+	// 验证失败
+	CCustomMessageBox dlg(_T("验证失败，请重新输入"), RGB(255, 0, 0), this);
+	dlg.DoModal();
+	pMainDlg->UpdateStatistics(false);
+	m_captchaInput.SetWindowText(_T(""));
+}
+	}
+}
+
+void CCaptchaDlg::OnBnClickedCloseCaptcha()
+{
+	KillTimer(1);
+	EndDialog(IDCANCEL);
+}
+
+// CStatisticsDlg 实现
+CStatisticsDlg::CStatisticsDlg(int correctCount, int wrongCount, CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_STATISTICS_DIALOG, pParent), m_correctCount(correctCount), m_wrongCount(wrongCount)
+{
+}
+
+void CStatisticsDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_CORRECT_COUNT, m_correctDisplay);
+	DDX_Control(pDX, IDC_WRONG_COUNT, m_wrongDisplay);
+}
+
+BEGIN_MESSAGE_MAP(CStatisticsDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_CLOSE_STATISTICS, &CStatisticsDlg::OnBnClickedCloseStatistics)
+	ON_WM_CTLCOLOR()
+END_MESSAGE_MAP()
+
+BOOL CStatisticsDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置统计信息
+	CString correctStr, wrongStr;
+	correctStr.Format(_T("%d"), m_correctCount);
+	wrongStr.Format(_T("%d"), m_wrongCount);
+	m_correctDisplay.SetWindowText(correctStr);
+	m_wrongDisplay.SetWindowText(wrongStr);
+
+	return TRUE;
+}
+
+HBRUSH CStatisticsDlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	HBRUSH hbr = CDialogEx::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	if (pWnd->GetDlgCtrlID() == IDC_CORRECT_COUNT)
+	{
+		pDC->SetTextColor(RGB(0, 255, 0));
+		pDC->SetBkColor(GetSysColor(COLOR_WINDOW));
+		return (HBRUSH)GetStockObject(WHITE_BRUSH);
+	}
+	else if (pWnd->GetDlgCtrlID() == IDC_WRONG_COUNT)
+	{
+		pDC->SetTextColor(RGB(255, 0, 0));
+		pDC->SetBkColor(GetSysColor(COLOR_WINDOW));
+		return (HBRUSH)GetStockObject(WHITE_BRUSH);
+	}
+
+	return hbr;
+}
+
+void CStatisticsDlg::OnBnClickedCloseStatistics()
+{
+	EndDialog(IDCANCEL);
+}
+
 // CMFCApplication2Dlg 对话框
 
 
 
 CMFCApplication2Dlg::CMFCApplication2Dlg(CWnd* pParent /*=nullptr*/)
-	: CDialogEx(IDD_MFCAPPLICATION2_DIALOG, pParent)
+	: CDialogEx(IDD_MFCAPPLICATION2_DIALOG, pParent), m_correctCount(0), m_wrongCount(0)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
@@ -69,6 +277,8 @@ BEGIN_MESSAGE_MAP(CMFCApplication2Dlg, CDialogEx)
 	ON_BN_CLICKED(IDOK, &CMFCApplication2Dlg::OnBnClickedOk)
 	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication2Dlg::OnBnClickedButton1)
 	ON_WM_TIMER()
+	ON_BN_CLICKED(IDC_BUTTON3, &CMFCApplication2Dlg::OnBnClickedButton3)
+	ON_BN_CLICKED(IDC_BUTTON4, &CMFCApplication2Dlg::OnBnClickedButton4)
 END_MESSAGE_MAP()
 
 
@@ -193,5 +403,26 @@ void CMFCApplication2Dlg::OnBnClickedButton1()
 	wnd->SetWindowText(text + _T("Hello, MFC!"));
 	*/
 		
+}
+
+// CMFCApplication2Dlg 新功能实现
+void CMFCApplication2Dlg::UpdateStatistics(bool isCorrect)
+{
+	if (isCorrect)
+		m_correctCount++;
+	else
+		m_wrongCount++;
+}
+
+void CMFCApplication2Dlg::OnBnClickedButton3()
+{
+	CCaptchaDlg dlg(this);
+	dlg.DoModal();
+}
+
+void CMFCApplication2Dlg::OnBnClickedButton4()
+{
+	CStatisticsDlg dlg(m_correctCount, m_wrongCount, this);
+	dlg.DoModal();
 }
 
--- MFCApplication2Dlg.h
+++ MFCApplication2Dlg.h
@@ -1,38 +1,110 @@
-﻿
+
 // MFCApplication2Dlg.h: 头文件
 //
 
 #pragma once
 
 
+// 验证码对话框类
+class CCustomMessageBox : public CDialogEx
+{
+public:
+	CCustomMessageBox(const CString& message, COLORREF textColor, CWnd* pParent = nullptr);
+	virtual ~CCustomMessageBox();
+
+protected:
+	virtual BOOL OnInitDialog();
+	virtual void DoDataExchange(CDataExchange* pDX);
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	DECLARE_MESSAGE_MAP()
+
+private:
+	CString m_message;
+	COLORREF m_textColor;
+	CStatic m_messageText;
+};
+
+// 验证码对话框类
+
+class CCaptchaDlg : public CDialogEx
+{
+public:
+	CCaptchaDlg(CWnd* pParent = nullptr);
+	
+	enum { IDD = IDD_CAPTCHA_DIALOG };
+
+protected:
+	virtual BOOL OnInitDialog();
+	virtual void DoDataExchange(CDataExchange* pDX);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnEnChangeCaptchaInput();
+	afx_msg void OnBnClickedCloseCaptcha();
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	DECLARE_MESSAGE_MAP()
+
+private:
+	CString m_captchaCode;
+	CStatic m_captchaDisplay;
+	CEdit m_captchaInput;
+	void GenerateCaptchaCode();
+};
+
+// 统计信息对话框类
+class CStatisticsDlg : public CDialogEx
+{
+public:
+	CStatisticsDlg(int correctCount, int wrongCount, CWnd* pParent = nullptr);
+	
+	enum { IDD = IDD_STATISTICS_DIALOG };
+
+protected:
+	virtual BOOL OnInitDialog();
+	virtual void DoDataExchange(CDataExchange* pDX);
+	afx_msg void OnBnClickedCloseStatistics();
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	DECLARE_MESSAGE_MAP()
+
+private:
+	int m_correctCount;
+	int m_wrongCount;
+	CStatic m_correctDisplay;
+	CStatic m_wrongDisplay;
+};
+
 // CMFCApplication2Dlg 对话框
 class CMFCApplication2Dlg : public CDialogEx
 {
 // 构造
 public:
-	CMFCApplication2Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+	CMFCApplication2Dlg(CWnd* pParent = nullptr);
 
 // 对话框数据
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_MFCAPPLICATION2_DIALOG };
 #endif
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
-
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
 
 // 实现
 protected:
 	HICON m_hIcon;
+	int m_correctCount;
+	int m_wrongCount;
 
 	// 生成的消息映射函数
 	virtual BOOL OnInitDialog();
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton4();
 	DECLARE_MESSAGE_MAP()
+
 public:
 	afx_msg void OnBnClickedOk();
 	afx_msg void OnBnClickedButton1();
 	void OnSize(UINT nType, int cx, int cy);
+	void UpdateStatistics(bool isCorrect);
 };
+
